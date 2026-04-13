c96892a892a1e8682b9a146b155456529520e6cb
Student <Student@LabVM2049939>
2026-04-13 12:44:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+
+
+namespace prompt_user
+{//start of namespace
+    public class Program
+    {//start of class 
+
+        static void Main(string[] args)
+        {//start of main method
+         //creating an instance of the class prompt_username with an object name ask_name
+         //to create an instance you start by the name of the class,then follows by the object name ,then follows by new keyword and repeat the name of the class and put both () and ;
+            prompt_username ask_name = new prompt_username();
+
+
+
+
+
+        }//end of main method 
+    }//end of class
+}//end of namespace 
+// never make the main method private,
---
+++ prompt_username.cs
@@ -0,0 +1,40 @@
+﻿namespace prompt_user
+{//opening of namespace
+    public class prompt_username
+    {//opening of class
+    //global variable declaration
+    
+        private string username = string.Empty;
+
+
+        //void method to prompt user 
+        //to craete a method we start by using an access modifer and then follows nby the type of method and after that the name of the methid follow and put () aD {}
+        public void collect_username()
+        {//start of colect_username 
+
+
+
+        }// end of collect_username method
+        //boolean method to check if username is entered or not 
+        private Boolean check_username ()
+        {//start of check_username
+            //if statement check if user has entered their  name or not
+
+            if(username != "" )//empty quotation is = to user ha  not entered naything 
+            
+            {//start of if
+                //return true and the message 
+                Console.WriteLine("Chatbot : Hey" + username + ", ho wcan i help you ?");//then return
+                return true;
+
+                }//end of if
+            else
+            {//start of else
+             //error message 
+                Console.WriteLine("Chatbot: Please enter your name ");
+                return false;
+            }//end of else
+
+        }//end of  username 
+    }//end of class
+}//end of namespace
